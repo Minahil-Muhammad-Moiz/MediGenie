9d012aada8bc202153392599ddb9bac926c55579
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2025-04-30 17:23:31 +0500

    react-native-picker-select

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       colors: {
         black1: "#09090b",
         blue1: '#22d3ee',
-        darkGrey: "#171717;",
+        darkGrey: "#171717",
         lightText: '#d4d4d4',
         lightGrey: "#9ca3af",
         fail: "#ef4444",
