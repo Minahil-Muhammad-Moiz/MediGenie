c4319f661f7ff0735ab73c2b18d301120f34bc39
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2025-06-02 01:07:59 +0500

    created second screen for information, Medical Histor screen

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import HomeScreen from './screens/HomeScreen';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import ProfileScreen from './screens/ProfileScreen';
+import MedicalHistory from './screens/MedicalHistory';
 
 
 const App = () => {
@@ -27,21 +28,22 @@ const App = () => {
           showHideTransition="fade"
           hidden={true}
         />
-        <ProfileScreen/>
-        {/* <Stack.Navigator initialRouteName="GettingStarted"
+        <Stack.Navigator 
+        // initialRouteName="GettingStarted"
           screenOptions={{
             headerShown: false
           }}
         >
-          <Stack.Screen name="GettingStarted" component={StartScreen} />
-          <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+          {/* <Stack.Screen name="GettingStarted" component={StartScreen} /> */}
+          {/* <Stack.Screen name="SignUpScreen" component={SignUpScreen} /> */}
           <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="MedicalHistory" component={MedicalHistory} />
+          {/* <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
           <Stack.Screen name='EmailVerification' component={EmailVerification} />
           <Stack.Screen name='ResetPassword' component={ResetPassword} />
-          <Stack.Screen name='HomeScreen' component={HomeScreen} />
-        </Stack.Navigator> */}
+          <Stack.Screen name='HomeScreen' component={HomeScreen} /> */}
+        </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   )
---
+++ src/screens/MedicalHistory.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MedicalHistory = () => {
+    return (
+        <KeyboardAvoidingContainer>
+            <MainContainer>
+            </MainContainer>
+        </KeyboardAvoidingContainer>
+    )
+}
+
+export default MedicalHistory
