07124eef5e33054366c9abad844f08b3dde78f9f
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2025-06-06 00:58:27 +0500

    hero div styling

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -8,18 +8,19 @@ import { useSelector } from 'react-redux';
 
 
 const HomeScreen = () => {
+    const imageURI = require('../assets/images/logoBlack.png')
     const profileImage = useSelector((state) => state.profile.profileImage);
 
     return (
         <>
             <View className='bg-black1 relative'>
 
-                <View className='flex flex-row items-center justify-between w-full bg-blue1 p-7 pb-20 rounded-b-3xl'>
+                <View className='flex flex-row items-center justify-between w-full  p-7 rounded-b-3xl'>
                     <View className='flex flex-row items-center justify-center gap-4 '>
                         <Image source={profileImage} height={10} width={10} alt='dummy-profile' className=' w-16 h-16 rounded-full ' />
                         <View className='flex justify-center'>
-                            <Text className={`text-zinc-700 text`}>Welcome</Text>
-                            <Text className={`text-black font-bold text-3xl`}>Antonio 👋</Text>
+                            <Text className={`font-poppins text-lightText text`}>Welcome</Text>
+                            <Text className={`text-white font-poppins font-bold text-3xl`}>Antonio 👋</Text>
 
                         </View>
 
@@ -31,7 +32,7 @@ const HomeScreen = () => {
                     >
                         <Ionicons
                             name={'ellipsis-vertical'}
-                            color={colors.black1}
+                            color={colors.blue1}
                             size={25}
                         />
                     </TouchableOpacity>
@@ -39,13 +40,17 @@ const HomeScreen = () => {
 
                 </View>
 
-                <View className='max-w-[90%] p-2 bg-white absolute top-[100%] -translate-y-[30%] -translate-x-[50%] left-[50%] z-10'>
-                    <Text>Your all-in-one companion to manage and understand your health better. Get started by checking your symptoms, scanning reports, or uploading PDFs.</Text>
-                </View>
+                {/* absolute top-[100%] -translate-y-[30%] -translate-x-[50%] left-[50%]  */}
             </View>
 
             <KeyboardAvoidingContainer>
                 <MainContainer>
+                    <View className=' mx-auto font-poppinsRegular max-w-[90%] rounded-xl bg-blue1 overflow-hidden z-10 relative pr-20'>
+                        <Text className='z-10 text-3xl font-poppinsBold '>MediGenie</Text>
+                        <Text className='z-10 text italic'>Your all-in-one companion to manage and understand your health better. </Text>
+                        <Image source={imageURI} height={10} width={10} alt='dummy-profile' className=' w-28 h-28 absolute right-0 bottom-0 z-0' />
+
+                    </View>
                 </MainContainer>
             </KeyboardAvoidingContainer>
         </>
