beb7eb0cff5575d50d96edb47374e81e528e91dc
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2025-06-06 10:48:21 +0500

    feature cards

--- src/components/DefaultButton.jsx
+++ src/components/DefaultButton.jsx
@@ -1,10 +1,18 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { colors } from '../utils/constants';
 
 const DefaultButton = ({ ...props }) => {
     return (
-        <TouchableOpacity {...props} className={`w-full rounded-full  ${props?.fill ? 'bg-blue1' : 'bg-black1'}  ${props?.border && 'border border-blue1'}`} onPress={props?.onPress} title={props?.title}>
-            <Text className='font-poppinsBold font-bold text-base p-4 text-center text-black1'>{props?.children}</Text>
+        <TouchableOpacity {...props} className={`w-full rounded-full ${props?.thinPadding ? 'px-4 py-3' : 'p-4'} ${props?.fill ? 'bg-blue1' : 'bg-black1'}  ${props?.border && 'border border-blue1'} flex-row items-center justify-center gap-2`} onPress={props?.onPress} title={props?.title}>
+            <Text className={`font-poppinsBold font-bold text-base  text-center ${props?.textWhite ? 'text-white' : 'text-black1'}`}>{props?.children}</Text>
+            {props?.icon ?
+                <Ionicons
+                    name={props?.icon}
+                    color={colors.black1}
+                    size={25}
+                /> : null}
         </TouchableOpacity>
     )
 }
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -5,6 +5,7 @@ import MainContainer from '../components/MainContainer'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { colors } from '../utils/constants';
 import { useSelector } from 'react-redux';
+import DefaultButton from '../components/DefaultButton';
 
 
 const HomeScreen = () => {
@@ -15,15 +16,13 @@ const HomeScreen = () => {
         <>
             <View className='bg-black1 relative'>
 
-                <View className='flex flex-row items-center justify-between w-full  p-7 rounded-b-3xl'>
+                <View className='flex flex-row items-center justify-between w-full  p-6 rounded-b-3xl'>
                     <View className='flex flex-row items-center justify-center gap-4 '>
                         <Image source={profileImage} height={10} width={10} alt='dummy-profile' className=' w-16 h-16 rounded-full ' />
                         <View className='flex justify-center'>
                             <Text className={`font-poppins text-lightText text`}>Welcome</Text>
                             <Text className={`text-white font-poppins font-bold text-3xl`}>Antonio 👋</Text>
-
                         </View>
-
                     </View>
 
                     <TouchableOpacity
@@ -45,12 +44,52 @@ const HomeScreen = () => {
 
             <KeyboardAvoidingContainer>
                 <MainContainer>
-                    <View className=' mx-auto font-poppinsRegular max-w-[90%] rounded-xl bg-blue1 overflow-hidden z-10 relative pr-20'>
-                        <Text className='z-10 text-3xl font-poppinsBold '>MediGenie</Text>
-                        <Text className='z-10 text italic'>Your all-in-one companion to manage and understand your health better. </Text>
-                        <Image source={imageURI} height={10} width={10} alt='dummy-profile' className=' w-28 h-28 absolute right-0 bottom-0 z-0' />
 
+                    <View className='p-10  mb-6 mx-auto font-poppinsRegular w-[90%] rounded-xl bg-blue1 overflow-hidden z-10 relative'>
+                        <Text className='z-10 text-3xl font-poppinsBold font-extrabold'>MediGenie</Text>
+                        <Text className='z-10 text italic max-w-[60%]'>Your Health, simplifiied with AI.</Text>
+                        <Image source={imageURI} height={10} width={10} alt='dummy-profile' className=' w-24 h-24 absolute right-0 bottom-0  z-0' />
                     </View>
+
+                    <View className='flex gap-4 items-center justify-start border bord'>
+
+                        <View className=' h-52 border border-blue1 mx-auto p-5 rounded-2xl flex items-start justify-around'>
+                            <View className='max-w-[90%] flex gap-2'>
+                                <Text className='font-poppins text-white font-bold text-2xl'>Symptom Checker</Text>
+                                <Text className='font-poppins text-white'>Upload an image or type your  symptoms to get an instant analysis.</Text>
+                            </View>
+                            <View className='self-end w-40 '>
+                                <DefaultButton border fill thinPadding icon={'arrow-forward-circle'}>
+                                    Try Now
+                                </DefaultButton>
+                            </View>
+                        </View>
+
+                        <View className=' h-52 border border-blue1 mx-auto p-5 rounded-2xl flex items-start justify-around'>
+                            <View className='max-w-[90%] flex gap-2'>
+                                <Text className='font-poppins text-white font-bold text-2xl'>Report Scanner</Text>
+                                <Text className='font-poppins text-white'>Scan and organize your health records with ease.</Text>
+                            </View>
+                            <View className='self-end w-40 '>
+                                <DefaultButton border fill thinPadding icon={'arrow-forward-circle'}>
+                                    Try Now
+                                </DefaultButton>
+                            </View>
+                        </View>
+
+                        <View className=' h-52 border border-blue1 mx-auto p-5 rounded-2xl flex items-start justify-around'>
+                            <View className='max-w-[90%] flex gap-2'>
+                                <Text className='font-poppins text-white font-bold text-2xl'>PDF Analyzer</Text>
+                                <Text className='font-poppins text-white'>Upload medical reports and get a summarized overview instantly.</Text>
+                            </View>
+                            <View className='self-end w-40 '>
+                                <DefaultButton border fill thinPadding icon={'arrow-forward-circle'}>
+                                    Try Now
+                                </DefaultButton>
+                            </View>
+                        </View>
+                    </View>
+
                 </MainContainer>
             </KeyboardAvoidingContainer>
         </>
