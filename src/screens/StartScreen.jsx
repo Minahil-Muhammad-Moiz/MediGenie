56dec952491da965429b75201d05c519208474b2
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2025-06-15 23:58:33 +0500

    changes starting screen UI

--- src/screens/StartScreen.jsx
+++ src/screens/StartScreen.jsx
@@ -22,20 +22,20 @@ export default function StartScreen() {
             <View className=' flex items-center justify-center h-full w-full relative'>
                 {/* logo header */}
                 <View className='flex items-center'>
-                    <Image source={imageURI} height={10} width={10} alt='logo' className='h-44 w-44' />
-                    <Text className='text-white text-5xl font-extrabold font-poppinsBold'>MediGenie</Text>
-                    <Text className='text-center text-lightGrey text-xl font-poppins'>Where AI Meets Personalized Healthcare</Text>
+                    <Image source={imageURI} height={10} width={10} alt='logo' className='h-36 w-36 -mt-2 mb-2' />
+                    <Text className='text-white text-3xl font-extrabold font-poppinsBold'>MediGenie</Text>
+                    <Text className='text-center text-lightGrey text-base font-poppins'>Where AI Meets Personalized Healthcare</Text>
                 </View>
 
                 {/* login/signup buttons */}
-                <View className=' flex flex-row w-[90%] items-center absolute bottom-[5%] justify-center bg-darkGrey rounded-[3rem] overflow-hidden border-blue1 border'>
+                <View className=' flex flex-row w-[80%] items-center absolute bottom-[5%] justify-center bg-darkGrey rounded-[3rem] overflow-hidden border-blue1 border'>
 
-                    <TouchableOpacity className='rounded-[3rem] p-3 text-center border-blue1 border bg-blue1 w-[50%]' onPress={handleLogin}>
-                        <Text className='text-black text-center font-bold text-lg font-poppinsRegular'>LOG IN</Text>
+                    <TouchableOpacity className='rounded-[3rem] p-2 text-center border-blue1 border bg-blue1 w-[50%]' onPress={handleLogin}>
+                        <Text className='text-black text-center font-bold text-base font-poppinsRegular'>LOG IN</Text>
                     </TouchableOpacity >
 
-                    <TouchableOpacity className='w-[50%] rounded-3xl bg-darkGrey p-3' onPress={handleSignUp}>
-                        <Text className='text-white text-center font-bold text-lg font-poppinsRegular'>SIGN UP</Text>
+                    <TouchableOpacity className='w-[50%] rounded-3xl bg-darkGrey p-2' onPress={handleSignUp}>
+                        <Text className='text-white text-center font-bold text-base font-poppinsRegular'>SIGN UP</Text>
                     </TouchableOpacity>
 
 
